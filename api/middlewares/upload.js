e30116c528771aac0df9fd470cbfdf054253ff8e
meandro02 <rd209422@gmail.com>
2024-01-12 13:04:56 -0600

    prueba  subida un prod local midleware  01 12

--- api/middlewares/upload.js
+++ api/middlewares/upload.js
@@ -1,17 +1,40 @@
 const multer = require('multer');
 const fs = require('fs');
 
+// const storage = multer.diskStorage({
+//   destination: function(req, file, cb) {
+//     console.log(req);
+//     const path = `./uploads/${req.params.category}/${req.params.id}`;
+//   fs.mkdirSync(path, { recursive: true })
+//   fs.mkdir(path, err => cb(null, path));
+//   },
+//   filename: function(req, file, cb) {
+//     cb(null, file.originalname);
+//   }
+// });
+
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    console.log(req);
-    const path = `./uploads/${req.params.category}/${req.params.id}`;
-  fs.mkdirSync(path, { recursive: true })
-  fs.mkdir(path, err => cb(null, path));
+    const basePath = './uploads';
+    const categoryPath = `${basePath}/${req.params.category}`;
+    const idPath = `${categoryPath}/${req.params.id}`;
+
+    try {
+      fs.mkdirSync(basePath, { recursive: true });
+      fs.mkdirSync(categoryPath, { recursive: true });
+      fs.mkdirSync(idPath, { recursive: true });
+
+      cb(null, idPath);
+    } catch (error) {
+      console.error(`Error creating directories: ${error.message}`);
+      cb(error, null);
+    }
   },
   filename: function(req, file, cb) {
     cb(null, file.originalname);
   }
 });
+
 
 const imageFileFilter = (req, file, cb) => {
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/webp' || file.mimetype === 'image/jpg') {
@@ -32,30 +55,6 @@ const pdfFileFilter = (req, file, cb) => {
 
 
 
-
-// Function to delete a directory and its contents recursively
-// const deleteDirectory = async (path) => {
-//   const unlink = util.promisify(fs.unlink);
-//   const rmdir = util.promisify(fs.rmdir);
-//   const readdir = util.promisify(fs.readdir);
-
-//   try {
-//     const files = await readdir(path);
-//     await Promise.all(files.map(async (file) => {
-//       const filePath = `${path}/${file}`;
-//       const stat = await fs.promises.stat(filePath);
-//       if (stat.isDirectory()) {
-//         await deleteDirectory(filePath);
-//       } else {
-//         await unlink(filePath);
-//       }
-//     }));
-//     await rmdir(path);
-//     console.log(`Directory deleted: ${path}`);
-//   } catch (error) {
-//     console.error(`Error deleting directory ${path}: ${error.message}`);
-//   }
-// };
 
 const maxFileSize = 1024 * 1024 * 5; // 5MB
 
