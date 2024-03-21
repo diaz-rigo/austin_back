6d55bdaf84488eb8884dc96267a87f3245b2f4b7
meandro02 <rd209422@gmail.com>
2024-03-21 01:11:57 -0600

    invitacion a si rol es != invitado

--- api/controllers/order.js
+++ api/controllers/order.js
@@ -96,27 +96,27 @@ exports.updateStatusOrder = async (req, res, next) => {
       to: userEmail,
       subject: 'Seguimiento de tu Pedido - Pastelería Austin\'s',
       html: `
-      <div style="background-color: #f5f5f5; padding: 20px; font-family: 'Arial', sans-serif;">
-        <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 10px; box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);">
-          <div style="text-align: center; padding: 20px;">
-            <img src="https://static.wixstatic.com/media/64de7c_4d76bd81efd44bb4a32757eadf78d898~mv2_d_1765_2028_s_2.png" alt="Austin's Logo" style="max-width: 100px;">
+        <div style="background-color: #f5f5f5; padding: 20px; font-family: 'Arial', sans-serif;">
+          <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 10px; box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);">
+            <div style="text-align: center; padding: 20px;">
+              <img src="https://static.wixstatic.com/media/64de7c_4d76bd81efd44bb4a32757eadf78d898~mv2_d_1765_2028_s_2.png" alt="Austin's Logo" style="max-width: 100px;">
+            </div>
+            <div style="text-align: center; padding: 20px;">
+              <h2 style="font-size: 24px; color: #333;">¡Gracias por tu compra en Pastelería Austin's!</h2>
+              <p style="color: #555; font-size: 16px;">Tu pedido ha sido procesado con éxito y pronto estará en camino. A continuación, te proporcionamos el número de seguimiento de tu pedido y las instrucciones para consultar su estado:</p>
+              <p style="font-weight: bold; font-size: 16px;">Número de Seguimiento: ${paypalOrderId}</p>
+              <p style="color: #555; font-size: 16px;">Instrucciones para consultar el estado del pedido:</p>
+              <ol style="color: #555; font-size: 16px;">
+                <li>Ingresa a nuestro sitio web.</li>
+                <li>Ve a la sección de "Seguimiento de Pedidos" o "Mis Pedidos".</li>
+                <li>Ingresa el número de seguimiento proporcionado arriba.</li>
+                <li>Consulta el estado actualizado de tu pedido.</li>
+              </ol>
+            </div>
+            <p style="text-align: center; color: #777; font-size: 14px;">¡Esperamos que disfrutes de tu compra! Si tienes alguna pregunta o necesitas asistencia adicional, no dudes en ponerte en contacto con nuestro equipo de soporte.</p>
           </div>
-          <div style="text-align: center; padding: 20px;">
-            <h2 style="font-size: 24px; color: #333;">¡Gracias por tu compra en Pastelería Austin's!</h2>
-            <p style="color: #555; font-size: 16px;">Tu pedido ha sido procesado con éxito y pronto estará en camino. A continuación, te proporcionamos el número de seguimiento de tu pedido y las instrucciones para consultar su estado:</p>
-            <p style="font-weight: bold; font-size: 16px;">Número de Seguimiento: ${paypalOrderId}</p>
-            <p style="color: #555; font-size: 16px;">Instrucciones para consultar el estado del pedido:</p>
-            <ol style="color: #555; font-size: 16px;">
-              <li>Ingresa a nuestro sitio web.</li>
-              <li>Ve a la sección de "Seguimiento de Pedidos" o "Mis Pedidos".</li>
-              <li>Ingresa el número de seguimiento proporcionado arriba.</li>
-              <li>Consulta el estado actualizado de tu pedido.</li>
-            </ol>
-          </div>
-          <p style="text-align: center; color: #777; font-size: 14px;">¡Esperamos que disfrutes de tu compra! Si tienes alguna pregunta o necesitas asistencia adicional, no dudes en ponerte en contacto con nuestro equipo de soporte.</p>
         </div>
-      </div>
-    `,
+      `,
     };
     // Envío de correo electrónico
     const mailOptionsInvitacion = {
@@ -124,27 +124,27 @@ exports.updateStatusOrder = async (req, res, next) => {
       to: userEmail,
       subject: '¡Únete a Pastelería Austin\'s y disfruta de beneficios exclusivos!',
       html: `
-    <div style="background-color: #f5f5f5; padding: 20px; font-family: 'Arial', sans-serif;">
-      <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 10px; box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);">
-        <div style="text-align: center; padding: 20px;">
-          <img src="https://static.wixstatic.com/media/64de7c_4d76bd81efd44bb4a32757eadf78d898~mv2_d_1765_2028_s_2.png" alt="Austin's Logo" style="max-width: 100px;">
+      <div style="background-color: #f5f5f5; padding: 20px; font-family: 'Arial', sans-serif;">
+        <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 10px; box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);">
+          <div style="text-align: center; padding: 20px;">
+            <img src="https://static.wixstatic.com/media/64de7c_4d76bd81efd44bb4a32757eadf78d898~mv2_d_1765_2028_s_2.png" alt="Austin's Logo" style="max-width: 100px;">
+          </div>
+          <div style="text-align: center; padding: 20px;">
+            <h2 style="font-size: 24px; color: #333;">¡Únete a Pastelería Austin's y disfruta de beneficios exclusivos!</h2>
+            <p style="color: #555; font-size: 16px;">Gracias por elegirnos para tus compras en línea. Para ofrecerte una experiencia aún mejor, te invitamos a activar tu cuenta y disfrutar de los siguientes beneficios:</p>
+            <ul style="color: #555; font-size: 16px;">
+              <li>Acceso rápido y fácil a tu historial de pedidos.</li>
+              <li>Seguimiento en tiempo real de tus pedidos.</li>
+              <li>Ofertas especiales y descuentos personalizados.</li>
+              <li>Gestión sencilla de tus direcciones de envío y métodos de pago.</li>
+            </ul>
+            <p style="color: #555; font-size: 16px;">Regístrate ahora y aprovecha al máximo tus compras en línea con nosotros. ¡Es rápido, fácil y gratuito!</p>
+            <a  style="display: inline-block; padding: 10px 20px; background-color: #ff5733; color: #fff; text-decoration: none; border-radius: 5px;">Activar cuenta</a>
+          </div>
+          <p style="text-align: center; color: #777; font-size: 14px;">Si prefieres no activar tu cuenta en este momento, puedes ignorar este mensaje.</p>
         </div>
-        <div style="text-align: center; padding: 20px;">
-          <h2 style="font-size: 24px; color: #333;">¡Únete a Pastelería Austin's y disfruta de beneficios exclusivos!</h2>
-          <p style="color: #555; font-size: 16px;">Gracias por elegirnos para tus compras en línea. Para ofrecerte una experiencia aún mejor, te invitamos a activar tu cuenta y disfrutar de los siguientes beneficios:</p>
-          <ul style="color: #555; font-size: 16px;">
-            <li>Acceso rápido y fácil a tu historial de pedidos.</li>
-            <li>Seguimiento en tiempo real de tus pedidos.</li>
-            <li>Ofertas especiales y descuentos personalizados.</li>
-            <li>Gestión sencilla de tus direcciones de envío y métodos de pago.</li>
-          </ul>
-          <p style="color: #555; font-size: 16px;">Regístrate ahora y aprovecha al máximo tus compras en línea con nosotros. ¡Es rápido, fácil y gratuito!</p>
-          <a  style="display: inline-block; padding: 10px 20px; background-color: #ff5733; color: #fff; text-decoration: none; border-radius: 5px;">Activar cuenta</a>
-        </div>
-        <p style="text-align: center; color: #777; font-size: 14px;">Si prefieres no activar tu cuenta en este momento, puedes ignorar este mensaje.</p>
       </div>
-    </div>
-  `,
+    `,
     };
 
 
@@ -158,13 +158,27 @@ exports.updateStatusOrder = async (req, res, next) => {
         console.log('Correo electrónico enviado con éxito:', info.response);
       }
     });
-    transporter.sendMail(mailOptionsInvitacion, (error, info) => {
-      if (error) {
-        console.error('Error al enviar correo electrónico:', error);
-      } else {
-        console.log('Correo electrónico enviado con éxito:', info.response);
-      }
-    });
+    // Verificar si el usuario es un invitado (GUEST)
+    if (user.rol === 'GUEST') {
+      transporter.sendMail(mailOptionsInvitacion, (error, info) => {
+        if (error) {
+          console.error('Error al enviar correo electrónico:', error);
+        } else {
+          console.log('Correo electrónico enviado con éxito:', info.response);
+        }
+      });
+    } else {
+      console.log('El usuario es un invitado, no se enviará el correo de invitación.');
+      // Envío del correo de invitación solo si el usuario no es un invitado
+
+    }
+    // transporter.sendMail(mailOptionsInvitacion, (error, info) => {
+    //   if (error) {
+    //     console.error('Error al enviar correo electrónico:', error);
+    //   } else {
+    //     console.log('Correo electrónico enviado con éxito:', info.response);
+    //   }
+    // });
 
     await purchaseDetail.save();
     res.status(200).json({ message: 'Estado del detalle de compra actualizado correctamente' });
