51dde02440be7103ddeb00175215aeba1d314dc8
meandro02 <rd209422@gmail.com>
2024-04-16 13:25:40 -0600

    ip nav

--- api/controllers/logs.js
+++ api/controllers/logs.js
@@ -1,23 +1,35 @@
-const Log = require('../models/logs'); // Importa el modelo Logger
+const Log = require('../models/logs');
+const useragent = require('express-useragent');
 
 exports.saveRequestLogs = async (req, res, next) => {
     try {
-        // Crea un nuevo registro de log
+        // Obtener el User-Agent de la solicitud
+        const userAgent = useragent.parse(req.headers['user-agent']);
+
+        // Crear un nuevo registro de log
         const log = new Log({
-            date: new Date(), // Fecha y hora actual
-            message: `Request to ${req.method} ${req.originalUrl} from ${req.ip}`, // Descripción de la solicitud
-            level: 'INFO', // Nivel de log (puede ajustarse según tus necesidades)
+            date: new Date(),
+            message: `Request to ${req.method} ${req.originalUrl} from ${req.ip}`,
+            level: 'INFO',
+            // Agregar detalles del navegador al mensaje del log
+            userAgent: {
+                browser: userAgent.browser,
+                version: userAgent.version,
+                os: userAgent.os,
+                platform: userAgent.platform,
+                source: req.headers['user-agent']
+            }
         });
 
-        // Guarda el registro en la base de datos
+        // Guardar el registro en la base de datos
         await log.save();
         
-        // Llama a la siguiente función de middleware en la cadena
+        // Continuar con el siguiente middleware en la cadena
         next();
     } catch (error) {
-        // Maneja cualquier error que ocurra al guardar el registro
+        // Manejar cualquier error que ocurra al guardar el registro
         console.error('Error al guardar el registro de solicitud:', error);
-        // Continúa con el siguiente middleware (si lo hay) pero indica que ha ocurrido un error
+        // Continuar con el siguiente middleware, indicando que ocurrió un error
         next(error);
     }
 };
