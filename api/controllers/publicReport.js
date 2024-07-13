485e03ecfc61ba93f01189f60611c10df0bfcbbd
meandro02 <rd209422@gmail.com>
2024-07-13 14:13:13 -0600

    busqueda pedido y  orden

--- api/controllers/publicReport.js
+++ api/controllers/publicReport.js
@@ -24,6 +24,7 @@ const transporter = nodemailer.createTransport({
     pass: process.env.PASSMAIL,
   },
 });
+
 exports.consultaventas_or_pedidos_by_code = async (req, res, next) => {
   try {
     const { code } = req.query; // Se obtiene el código desde los parámetros de consulta
@@ -34,7 +35,7 @@ exports.consultaventas_or_pedidos_by_code = async (req, res, next) => {
 
     // Si no se encuentra un pedido, buscar una venta con el código proporcionado
     if (!pedido) {
-      let venta = await Venta.findOne({ paypalOrderID: code }).populate('details');
+      let venta = await Venta.findOne({ trackingNumber: code }).populate('details');
 
       if (venta) {
         // Aquí puedes acceder a los detalles de la venta a través de la propiedad "details" de la venta
@@ -100,47 +101,4 @@ exports.consultaventas_or_pedidos_by_code = async (req, res, next) => {
     res.status(500).json({ error: "Error al buscar pedido o venta" });
   }
 };
-
-
-// exports.consultaventas_or_pedidos_by_code = async (req, res, next) => {
-//   try {
-//     // const { code } = req.body; // Se obtiene el código desde el cuerpo de la solicitud
-//     // console.log(code)
-//     const { code } = req.query; // Se obtiene el código desde los parámetros de consulta
-//     console.log(code)
-//     // Buscar si existe un pedido con el código proporcionado
-//     let pedido = await Pedido.findOne({ codigoPedido: code });
-
-//     // Si no se encuentra un pedido, buscar una venta con el código proporcionado
-//     if (!pedido) {
-//       let  venta = await Venta.findOne({ paypalOrderID: code });
-//     }
-
-//     // Si se encontró un pedido o una venta, devolverlo como respuesta al cliente
-//     if (pedido) {
-//       // Acceder a los datos del usuario y los detalles del pedido
-//       const usuario = await User.findById(pedido.usuario);
-//       const detallePedido = await PedidoDetalle.findById(pedido.detallePedido[0]);
-
-//       // Construir el objeto de resultado con los datos del pedido y del usuario
-//       const resultado = {
-//         _id: pedido._id,
-//         codigoPedido: pedido.codigoPedido,
-//         usuario: usuario,
-//         detallePedido: detallePedido,
-//         estadoPedido: pedido.estadoPedido,
-//         createdAt: pedido.createdAt
-//       };
-
-//       res.status(200).json({ resultado });
-//     } else {
-//       // Si no se encuentra ni un pedido ni una venta con el código proporcionado, devolver un mensaje de error
-//       res.status(404).json({ error: "Pedido o venta no encontrado" });
-//     }
-//   } catch (error) {
-//     // Manejar cualquier error que ocurra durante la búsqueda
-//     console.error("Error al buscar pedido o venta:", error);
-//     res.status(500).json({ error: "Error al buscar pedido o venta" });
-//   }
-// };
 
