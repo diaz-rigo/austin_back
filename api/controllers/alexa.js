8f89cb98a21cbd46de89245c15c58243b93e1ccc
meandro02 <rd209422@gmail.com>
2024-07-17 08:41:00 -0600

    correo y push

--- api/controllers/alexa.js
+++ api/controllers/alexa.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const webpush = require('web-push');
+const PushSubscription = require('../models/pushSubscription'); // Importa el modelo de suscripción push
 
 "use strict";
 const nodemailer = require("nodemailer");
@@ -31,12 +32,7 @@ webpush.setVapidDetails(
   vapidKeys.privateKey
 );
 
-// // const Compra = require('../models/compra');
-// const mongoose = require('mongoose');
-// const User = require('./models/User'); // Asegúrate de ajustar el path según tu estructura de proyecto
-// const Product = require('./models/Product'); // Asegúrate de ajustar el path según tu estructura de proyecto
-// const Venta = require('./models/Venta'); // Asegúrate de ajustar el path según tu estructura de proyecto
-// const VentaDetail = require('./models/VentaDetail'); // Asegúrate de ajustar el path según tu estructura de proyecto
+
 const VentaDetail = require("../models/ventaDetail");
 const Venta = require("../models/ventaSchema");
 // const User = require("../models/user");
@@ -51,9 +47,11 @@ exports.CREATE_COMPRA = async (req, res) => {
 
         // Validar el usuario
         const user = await User.findById(userId);
-        if (!user) {
-            return res.status(404).json({ message: 'Usuario no encontrado.' });
-        }
+        // const isNewUser = !user;
+
+        // if (isNewUser) {
+        //     return res.status(404).json({ message: 'Usuario no encontrado.' });
+        // }
 
         // Validar el producto
         const product = await Product.findById(productId);
@@ -84,7 +82,7 @@ exports.CREATE_COMPRA = async (req, res) => {
             user: userId,
             details: ventaDetail._id,
             totalAmount: totalPrice,
-            stripeSessionID: null, // Aquí puedes manejar la integración con Stripe si lo necesitas
+            stripeSessionID: null,
             trackingNumber: generarCodigoPedido()
         });
         await venta.save();
@@ -92,15 +90,169 @@ exports.CREATE_COMPRA = async (req, res) => {
         // Actualizar el stock del producto
         product.stock -= quantity;
         await product.save();
+        const fechaActual = new Date().toLocaleString();
+
+        // Enviar notificación por correo si es la primera vez del usuario
+        // if (isNewUser) {
+            const mailOptionsSeguimiento = {
+                from: '"Pastelería Austin\'s" <austins0271142@gmail.com>',
+                to: user.email,
+                subject: '¡Tu pedido ha sido solicitado! - Pastelería Austin\'s',
+                html: `
+                    <div style="background-color: #f5f5f5; padding: 20px; font-family: 'Arial', sans-serif;">
+                        <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 10px; box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);">
+                            <div style="text-align: center; padding: 20px;">
+                                <img src="https://static.wixstatic.com/media/64de7c_4d76bd81efd44bb4a32757eadf78d898~mv2_d_1765_2028_s_2.png" alt="Logo de Pastelería Austin's" style="max-width: 100px;">
+                            </div>
+                            <div style="text-align: center; padding: 20px;">
+                                <p style="color: #555; font-size: 16px;">¡Gracias por confiar en Pastelería Austin's para tus deliciosos postres! Tu pedido ha sido solicitado con éxito y pronto nos comunicaremos.</p>
+                                <p style="font-weight: bold; font-size: 16px;">CÓDIGO DE PEDIDO: ${venta.trackingNumber} 🎂</p>
+                                <p style="color: #555; font-size: 16px;">Tu pedido fue realizado a través de Alexa el ${fechaActual}.</p>
+                                <p style="color: #555; font-size: 16px;">Sigue estos pasos para consultar el estado de tu pedido:</p>
+                                <ol style="color: #555; font-size: 16px;">
+                                    <li>Ingresa a nuestro <a href="https://austins.vercel.app">sitio web 🌐</a>.</li>
+                                    <li>Dirígete a la sección de "Seguimiento de Pedidos" o "Mis Pedidos".</li>
+                                    <li>Ingresa el número de pedido proporcionado arriba.</li>
+                                    <li>Consulta el estado actualizado de tu pedido.</li>
+                                </ol>
+                            </div>
+                            <p style="text-align: center; color: #777; font-size: 14px;">¡Esperamos que disfrutes de tu pedido! Si necesitas asistencia adicional, no dudes en ponerte en contacto con nuestro equipo de soporte. 🍰🎉</p>
+                        </div>
+                    </div>
+                `,
+            };
+
+            await enviarCorreo(mailOptionsSeguimiento);
+        // }
+
+        // Enviar notificación push si el usuario tiene suscripciones
+        if (user.subscriptions && user.subscriptions.length > 0) {
+            for (const subscriptionId of user.subscriptions) {
+                const subscription = await PushSubscription.findById(subscriptionId);
+                if (subscription) {
+                    const payload = {
+                        notification: {
+                            title: 'Seguimiento de tu Pedido 🍰',
+                            body: `¡Tu pedido realizado a través de Alexa ha sido solicitado! Sigue el estado con el código: ${venta.trackingNumber} 🎉`,
+                            icon: "https://static.wixstatic.com/media/64de7c_4d76bd81efd44bb4a32757eadf78d898~mv2_d_1765_2028_s_2.png",
+                            vibrate: [200, 100, 200],
+                            sound: 'https://res.cloudinary.com/dfd0b4jhf/video/upload/v1710830978/sound/kjiefuwbjnx72kg7ouhb.mp3',
+                            priority: 'high',
+                            data: {
+                                url: "https://austins.vercel.app"
+                            },
+                            actions: [
+                                { action: "ver_pedido", title: "Ver Pedido" },
+                            ],
+                            expiry: Math.floor(Date.now() / 1000) + 28 * 86400,
+                            timeToLive: 28 * 86400,
+                            silent: false
+                        }
+                    };
+
+                    try {
+                        await enviarNotificacionPush2(subscription, payload, user._id);
+                        console.log('Notificación push enviada exitosamente');
+                    } catch (error) {
+                        console.error('Error al enviar la notificación push:', error);
+                    }
+                }
+            }
+        }
 
         // Responder a la solicitud
-        res.status(200).json({ message: 'Compra creada exitosamente.', ventaId: venta._id });
+        res.status(200).json({ message: 'Compra pendiente creada exitosamente.', ventaId: venta._id });
 
     } catch (error) {
         console.error(`Error al crear la compra: ${error.message}`);
         res.status(500).json({ message: 'Error interno del servidor.' });
     }
 };
+
+
+
+// exports.CREATE_COMPRA = async (req, res) => {
+//     try {
+//         const { userId, productId, quantity, totalPrice } = req.body;
+
+//         if (!userId || !productId || !quantity || !totalPrice) {
+//             return res.status(400).json({ message: 'Faltan datos requeridos.' });
+//         }
+
+//         // Validar el usuario
+//         const user = await User.findById(userId);
+//         if (!user) {
+//             return res.status(404).json({ message: 'Usuario no encontrado.' });
+//         }
+
+//         // Validar el producto
+//         const product = await Product.findById(productId);
+//         if (!product) {
+//             return res.status(404).json({ message: 'Producto no encontrado.' });
+//         }
+
+//         if (product.stock < quantity) {
+//             return res.status(400).json({ message: 'Stock insuficiente.' });
+//         }
+
+//         // Crear el detalle de la venta
+//         const ventaDetail = new VentaDetail({
+//             _id: new mongoose.Types.ObjectId(),
+//             user: userId,
+//             products: [{
+//                 product: productId,
+//                 quantity: quantity,
+//             }],
+//             totalAmount: totalPrice,
+//             status: 'PENDING',
+//         });
+//         await ventaDetail.save();
+
+//         // Crear la venta
+//         const venta = new Venta({
+//             _id: new mongoose.Types.ObjectId(),
+//             user: userId,
+//             details: ventaDetail._id,
+//             totalAmount: totalPrice,
+//             stripeSessionID: null, // Aquí puedes manejar la integración con Stripe si lo necesitas
+//             trackingNumber: generarCodigoPedido()
+//         });
+//         await venta.save();
+
+//         // Actualizar el stock del producto
+//         product.stock -= quantity;
+//         await product.save();
+
+//         // Responder a la solicitud
+//         res.status(200).json({ message: 'Compra creada exitosamente.', ventaId: venta._id });
+
+//     } catch (error) {
+//         console.error(`Error al crear la compra: ${error.message}`);
+//         res.status(500).json({ message: 'Error interno del servidor.' });
+//     }
+// };
+
+const enviarNotificacionPush2 = async (subscription, payload, iduser) => {
+    try {
+      await webpush.sendNotification(subscription, JSON.stringify(payload));
+      console.log('Notificación push enviada con éxito');
+    } catch (error) {
+      console.error('Error al enviar notificación push:', error);
+      throw error;
+    }
+  };
+
+// Función para enviar correo electrónico
+const enviarCorreo = async (mailOptions) => {
+    try {
+      await transporter.sendMail(mailOptions);
+      console.log('Correo electrónico enviado con éxito:', mailOptions.to);
+    } catch (error) {
+      console.error('Error al enviar correo electrónico:', error);
+      throw error;
+    }
+  };
+
 const generarCodigoPedido = () => {
   const longitud = 6;
   const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
