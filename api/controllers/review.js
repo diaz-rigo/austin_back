d2d65bec456d18e3cfaabfe18f7c13e8224e501f
meandro02 <rd209422@gmail.com>
2024-07-21 00:19:11 -0600

    review

--- api/controllers/review.js
+++ api/controllers/review.js
@@ -1,29 +1,42 @@
 const mongoose = require('mongoose');
 const Product = require('../models/product');
 const Review = require('../models/review');
+const User = require("../models/user");
 
 exports.addReview = async (req, res, next) => {
-    const { productId, userId, comment, rating } = req.body;
-
-    if (!mongoose.Types.ObjectId.isValid(productId) || !mongoose.Types.ObjectId.isValid(userId)) {
-        return res.status(400).json({ message: 'Invalid product or user ID' });
-    }
-
+    const { productId, userId, email, comment, rating } = req.body;
+    console.log(req.body)
     try {
-        const review = new Review({
-            productId: new mongoose.Types.ObjectId(productId),
-            userId: new mongoose.Types.ObjectId(userId),
-            comment,
-            rating
-        });
-
-        await review.save();
-
-        res.status(201).json({ message: 'Review added successfully', review });
+        const usuario = await User.findOne({ email: email });
+        if (userId) {
+            const nuevaReseña = new Review({
+                productId: productId,
+                userId: userId,
+                comment: comment,
+                rating: rating
+            });
+            await nuevaReseña.save();
+            return res.status(201).json({ mensaje: 'Reseña agregada exitosamente.', reseña: nuevaReseña })
+        }else if(!userId && usuario){
+            const nuevaReseña = new Review({
+                productId: productId,
+                userId: usuario._id,
+                comment: comment,
+                rating: rating
+            });
+            await nuevaReseña.save();
+            return res.status(201).json({ mensaje: 'Reseña agregada exitosamente.', reseña: nuevaReseña })
+        }
+      
+        if (!usuario) {
+            return res.status(404).json({ mensaje: 'El usuario con el correo proporcionado no esta registrado en nuestra tienda.' });
+        }
     } catch (error) {
-        next(error);
+        console.error(error);
+        return res.status(500).json({ mensaje: 'Ocurrió un error al agregar la reseña.' });
     }
-};
+}
+
 
 exports.getProductReviews = async (req, res, next) => {
     const { productId } = req.params;
@@ -34,7 +47,7 @@ exports.getProductReviews = async (req, res, next) => {
 
     try {
         const reviews = await Review.find({ productId: new mongoose.Types.ObjectId(productId) })
-                                    .populate('userId', 'username'); // Assuming User model has 'username' field
+            .populate('userId', 'username'); // Assuming User model has 'username' field
 
         res.status(200).json(reviews);
     } catch (error) {
