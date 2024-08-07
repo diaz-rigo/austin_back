1a551cd0f9056ac4e8c953da48d422743580730d
meandro02 <rd209422@gmail.com>
2024-08-07 00:17:27 -0600

    pago de compra hecha

--- api/controllers/paymentStripe.js
+++ api/controllers/paymentStripe.js
@@ -109,9 +109,123 @@ exports.createSession = async (req, res) => {
     });
     await venta.save();
 
-    // const updatedSuccessUrl = `https://austins.vercel.app/payment/order-success?token=${session.id}`;
 
     return res.json({ url: session.url });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: error.message });
+  }
+};
+exports.createSession2 = async (req, res) => {
+  try {
+    const { totalneto, tipoEntrega, dateselect, productos, datoscliente, instruction, codigoDeSeguimiento, cancel_url } = req.body;
+
+    const isValidURL = (string) => {
+      try {
+        new URL(string);
+        return true;
+      } catch (_) {
+        return false;
+      }
+    };
+
+    const line_items = productos.map(producto => {
+      if (!isValidURL(producto.image)) {
+        throw new Error(`URL de imagen no válida: ${producto.image}`);
+      }
+
+      return {
+        price_data: {
+          currency: 'mxn',
+          product_data: {
+            name: producto.name,
+            images: [producto.image] // Asegúrate de que esto sea un array
+          },
+          unit_amount: producto.precio * 100,
+        },
+        quantity: producto.cantidad,
+      };
+    });
+
+    console.log('Detalles de line_items:', JSON.stringify(line_items, null, 2));
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      mode: "payment",
+      line_items,
+      success_url: "https://austins.vercel.app/payment/order-success?token={CHECKOUT_SESSION_ID}",
+      cancel_url: cancel_url,
+      customer_email: datoscliente.email,
+      metadata: {
+        tipoEntrega,
+        dateselect,
+        instruction,
+        empresa: "Austins",
+      },
+    });
+
+    let user;
+    
+    if (codigoDeSeguimiento) {
+      // Si ya hay un código de seguimiento, busca la venta existente
+      const existingVenta = await Venta.findOne({ trackingNumber: codigoDeSeguimiento }).populate('user');
+      
+      if (existingVenta) {
+        user = existingVenta.user; // Usa el usuario existente
+        
+        // Actualiza la venta existente con la nueva sesión de Stripe
+        existingVenta.stripeSessionID = session.id;
+        await existingVenta.save();
+        
+        return res.json({ url: session.url });
+      } else {
+        return res.status(404).json({ message: 'Venta con el código de seguimiento no encontrada' });
+      }
+    } else {
+      // Si no hay un código de seguimiento, crea un nuevo usuario y venta
+      user = await User.findOne({ email: datoscliente.email });
+      
+      if (!user) {
+        user = new User({
+          _id: new mongoose.Types.ObjectId(),
+          name: datoscliente.name,
+          paternalLastname: datoscliente.paternalLastname,
+          maternalLastname: datoscliente.maternalLastname,
+          phone: datoscliente.phone,
+          email: datoscliente.email,
+          rol: 'GUEST',
+          password: 'contraseñaPorDefecto',
+        });
+        await user.save();
+      }
+
+      const ventaDetail = new VentaDetail({
+        _id: new mongoose.Types.ObjectId(),
+        user: user._id,
+        products: productos.map(producto => ({
+          product: producto.id,
+          quantity: producto.cantidad,
+        })),
+        totalAmount: totalneto,
+        deliveryType: tipoEntrega,
+        deliveryDate: dateselect,
+        instruction: instruction,
+        status: 'PENDING',
+      });
+      await ventaDetail.save();
+
+      const venta = new Venta({
+        _id: new mongoose.Types.ObjectId(),
+        user: user._id,
+        details: ventaDetail._id,
+        totalAmount: totalneto,
+        stripeSessionID: session.id,
+        trackingNumber: generarCodigoPedido()
+      });
+      await venta.save();
+
+      return res.json({ url: session.url });
+    }
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: error.message });
